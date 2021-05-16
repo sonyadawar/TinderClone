bb1c02d0523be5663ff4dc0465ac778df86e4234
sonyadawar <44790119+sanchitdawar@users.noreply.github.com>
2021-05-17 00:21:03 +0530

    Update files

--- tinder-c/src/components/header/Header.js
+++ tinder-c/src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
-import logo from './assets/logo.png'
+import logo from '../../assets/logo.png'
 
 function Header() {
     return (
